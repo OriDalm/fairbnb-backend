78645c2ee06ea44476709b45c1c1e1a9adce8de0
OriDalm <oridalmedigos@gmail.com>
2023-11-03 15:06:36 +0200

    Changed env

--- config/dev.js
+++ config/dev.js
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv'
 
 dotenv.config()
-
+const password = process.env.SECRET 
 export default  {
-  dbURL: process.env.MONGO_URL,
+  dbURL:`mongodb+srv://oridalm:${password}@fairbnb.yw2ugh3.mongodb.net/?retryWrites=true&w=majority`,
   dbName : 'stay_db'
 }
--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,8 @@
-export default {
-  dbURL: process.env.MONGO_URL, 
-  dbName : process.env.DB_NAME || 'stay_db'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const password = process.env.SECRET 
+export default  {
+  dbURL:`mongodb+srv://oridalm:${password}@fairbnb.yw2ugh3.mongodb.net/?retryWrites=true&w=majority`,
+  dbName : 'stay_db'
 }
