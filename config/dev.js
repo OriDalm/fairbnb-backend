fef8a1287c36e1f077be00f24b765dffcc2dfb0f
Avishai Cohen <avishaic2016@gmail.com>
2023-11-05 16:56:28 +0200

    dbURL test

--- config/dev.js
+++ config/dev.js
@@ -1,11 +1,8 @@
 import dotenv from 'dotenv'
 
 dotenv.config()
-const mongoPassword = process.env.MONGO_URL
 
 export default  {
-  // dbURL: process.env.MONGO_URL,
-  // dbURL: mongoPassword,
-  dbURL:  `mongodb+srv://oridalm:${mongoPassword}@fairbnb.yw2ugh3.mongodb.net/?retryWrites=true&w=majority`,
+  dbURL: process.env.MONGO_URL,
   dbName : 'stay_db'
 }
