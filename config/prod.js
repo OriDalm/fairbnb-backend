9a049a432ac69c3495e63e9a8f17135824097f6c
OriDalm <oridalmedigos@gmail.com>
2024-02-05 16:49:47 +0200

    App skeleton

--- config/prod.js
+++ config/prod.js
@@ -4,5 +4,5 @@ dotenv.config()
 const password = process.env.SECRET 
 export default  {
   dbURL:`mongodb+srv://oridalm:${password}@fairbnb.yw2ugh3.mongodb.net/?retryWrites=true&w=majority`,
-  dbName : 'stay_db'
+  dbName : 'league_db'
 }
